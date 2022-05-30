a91f99acc632178c0bd944437e2d421dbccf7464
tanmay <tanmaymehta222@gmail.com>
2022-05-30 13:41:15 +0530

    edited

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import About from './components/About';
+import AllInOne from './components/AllInOne';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Pricing from './components/Pricing';
+import Support from './components/Support';
+import Footer from './components/Footer';
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About/>
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>  
+  );
+}
+
+export default App;
+
